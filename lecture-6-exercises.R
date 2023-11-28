f6f9b2eeeb7286870c0bd7aac69f9e52292785c5
Daniel Robinson <113081861+danierob@users.noreply.github.com>
2023-11-28 14:35:05 -0800

    exercise 3

--- lecture-6-exercises.R
+++ lecture-6-exercises.R
@@ -8,6 +8,8 @@ grep('\'', levels(gDat$country), value = TRUE)
 print("a\nb")
 cat("a\nb")
 
+
+#QUANTIFIERS
 (strings <- c("a", "ab", "acb", "accb", "acccb", "accccb"))
 grep("ac*b", strings, value = TRUE)
 grep("ac+b", strings, value = TRUE)
@@ -24,4 +26,28 @@ grep('ee', levels(gDat$country), value = TRUE)
 #Hint: use dir() to get a vector of all filenames, and make sure your .Rproj file is not included!
 grep('.R$', dir("C:/Users/d/Desktop/UBC 2023/548O/548O-danierob"), value = TRUE)
 
-#
+
+#OPERATORS
+(strings <- c("^ab", "ab", "abc", "abd", "abe", "ab 12"))
+grep("ab.", strings, value = TRUE)
+grep("ab[c-e]", strings, value = TRUE)
+grep("ab[^c]", strings, value = TRUE)
+grep("^ab", strings, value = TRUE)
+grep("\\^ab", strings, value = TRUE)
+grep("abc|abd", strings, value = TRUE)
+gsub("(ab) 12", "\\1 34", strings)
+
+
+
+#Exercise 3: Write code to find countries in Gapminder with letter i or t, and ends with
+#land, and replace land with LAND using backreference.
+#Hint: you may want to do this in 3 steps (i.e., this may not all need to be
+#achieved in a single line of code). If your final list includes Poland, something went wrong! If your final list does not include Finland, something went wrong!
+
+it <- grep("[it]", levels(gDat$country), value = TRUE, ignore.case = TRUE)
+land <- grep("land$", it, value = TRUE)
+gsub("land", "\\1LAND", land)
+
+
+
+
