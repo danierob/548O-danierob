ddd51d49d584e772a197edf0f5dd29842d86379f
Daniel Robinson <113081861+danierob@users.noreply.github.com>
2023-11-28 14:10:08 -0800

    Exercise 2! ".R$" the $ searches the end of word and keeps the .Rproj out

--- lecture-6-exercises.R
+++ lecture-6-exercises.R
@@ -16,7 +16,12 @@ grep("ac{2}b", strings, value = TRUE)
 grep("ac{2,}b", strings, value = TRUE)
 grep("ac{2,3}b", strings, value = TRUE)
 
-#exercise 1 : Write a line of code to find all countries with ee in Gapminder
+#Exercise 1 : Write a line of code to find all countries with ee in Gapminder
 #using quantifiers. Add, commit, and push.
 grep('ee', levels(gDat$country), value = TRUE)
 
+#Exercise 2 :Write code to find all .R files in the main folder of your repository.
+#Hint: use dir() to get a vector of all filenames, and make sure your .Rproj file is not included!
+grep('.R$', dir("C:/Users/d/Desktop/UBC 2023/548O/548O-danierob"), value = TRUE)
+
+#
