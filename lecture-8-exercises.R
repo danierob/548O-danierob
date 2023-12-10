558c01b4394904987e87f0adad1863cc7330750d
Daniel Robinson <113081861+danierob@users.noreply.github.com>
2023-12-10 12:10:10 -0800

    4!

--- lecture-8-exercises.R
+++ lecture-8-exercises.R
@@ -31,6 +31,7 @@ if (age_class == "sapling"){
 y
 
 #Exercise 3:
+#else if checks code conditions sequentially until one is met (TRUE)
 age_class <- "seedling"
 if (age_class == "sapling"){
   y <- 10
@@ -48,3 +49,65 @@ if (age_class == "sapling"){
   y <- 0
 }
 y
+
+
+#explaining the sequential check of 'else if' vs just plain old 'if'
+#this code returns two answers
+x <- 5
+if (x > 2){
+  x * 2
+}
+if (x > 4){
+  x * 4
+}
+
+#this code returns only the first
+x <- 5
+if (x > 2){
+  x * 2
+} else if (x > 4){
+  x * 4
+}
+
+#wrapping conditionals within a function
+est_mass <- function(volume, veg_type){
+  if (veg_type == "tree") {
+    mass <- 2.65 * volume^0.9
+  } else if (veg_type == "grass") {
+    mass <- 0.65 * volume^1.2
+  } else {
+    print("I don't know how to convert volume to mass for that vegetation type")
+    mass <- NA
+  }
+  return(mass)
+}
+
+est_mass(1.6, "tree")
+est_mass(1.6, "grass")
+est_mass(1.6, "shrub")
+
+#Exercise 4: dino mass = a*length^b
+get_mass_from_length_by_name <- function(length, name){
+  if (name == "stegosaura") {
+    mass <- 10.95*length^2.64
+  } else if (name == "theropoda") {
+    mass <- .73*length^3.63
+  } else if (name == "sauropoda") {
+    mass <- 214.44*length^1.46
+  } else {
+    mass <- NA
+  }
+  return(mass)
+}
+
+#A Stegosauria that is 10 meters long:
+get_mass_from_length_by_name(10, "stegosaura")
+
+#A Theropoda that is 8 meters long.
+get_mass_from_length_by_name(8, "theropoda")
+
+#A Sauropoda that is 12 meters long.
+get_mass_from_length_by_name(12, "sauropoda")
+
+#A Ankylosauria that is 13 meters long.
+get_mass_from_length_by_name(13, "ankylosauria")
