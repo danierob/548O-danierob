a6f8708985a3b23878d25ac86adf5bf43d558e42
Daniel Robinson <113081861+danierob@users.noreply.github.com>
2023-12-10 11:31:21 -0800

    Exercise 1

---
+++ lecture-8-exercises.R
@@ -0,0 +1,16 @@
+#LECTURE 8
+
+w <- 10.2
+x <- 1.3
+y <- 2.8
+z <- 17.5
+colors <- c("red", "blue", "green")
+masses <- c(45.2, 36.1, 27.8, 81.6, 42.4)
+dna1 <- "attattaggaccaca"
+dna2 <- "attattaggaacaca"
+
+#Exercise 1:
+w is greater than 10
+"green" is in colors
+x is greater than y
+Each value in masses is greater than 40.
