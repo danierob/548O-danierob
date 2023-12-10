88e4bfe99f7dcfaecdef3103aedb01680da3d179
Daniel Robinson <113081861+danierob@users.noreply.github.com>
2023-12-10 11:45:35 -0800

    exercise 3

--- lecture-8-exercises.R
+++ lecture-8-exercises.R
@@ -29,3 +29,22 @@ if (age_class == "sapling"){
   y <- 10
 }
 y
+
+#Exercise 3:
+age_class <- "seedling"
+if (age_class == "sapling"){
+  y <- 10
+} else if (age_class == "seedling"){
+  y <- 5
+}
+y
+
+age_class <- "adult"
+if (age_class == "sapling"){
+  y <- 10
+} else if (age_class == "seedling"){
+  y <- 5
+} else {
+  y <- 0
+}
+y
